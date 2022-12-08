bd49992cf5c0b18431aa9a8b40825f4233440396
Gonzalo Fabian Gomez <gonza_17_96_05@hotmail.com>
2022-12-08 05:32:23 -0300

    Creo las ordenes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,50 @@
 import { Link } from 'react-router-dom';
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
-
+import { collection, doc, setDoc, serverTimestamp, updateDoc, increment } from "firebase/firestore";
+import { db } from '../utils/firebaseConfig';
 
 
 const Cart = () => {
 
-    const test = useContext(CartContext);
+    const myContext = useContext(CartContext);
+
+    const createOrder = () => {
+
+        let order = {
+            buyer: {
+                name: 'Lionel Messi',
+                email: 'LeoMessi10@gmail.com',
+                phone: '1234567890'
+            },
+            total: myContext.calcTotal(),
+            items: myContext.cartList,
+            date: serverTimestamp()
+        }
+        
+        const createOrderInFirestore = async () => {
+            const newOrderRef = doc(collection(db, "orders"));
+            await setDoc(newOrderRef, order);
+            return newOrderRef;
+        }
+
+        createOrderInFirestore()
+        .then(result => {
+            alert('Su pedido ha sido creado. Por favor tome nota del ID de su pedido. ID: ' + result.id)
+
+            myContext.cartList.forEach(async (item) => {
+                const itemRef = doc(db, "products", item.id);
+                await updateDoc(itemRef, {
+                    stock: increment(-item.quantity)
+                });
+            });
+
+            myContext.clear();
+        })
+
+        .catch(err => console.log(err));
+        
+    }
 
     return (
         <>
@@ -15,8 +53,8 @@ const Cart = () => {
                 <div className="d-flex justify-content-evenly m-3">
                     <Link to='/'><button type="button" className="btn btn-primary">CONTINUAR COMPRANDO</button></Link>
                     {
-                        (test.cartList.length > 0)
-                        ? <button type="button" className="btn btn-danger" onClick={test.clear}>BORRAR TODO</button>
+                        (myContext.cartList.length > 0)
+                        ? <button type="button" className="btn btn-danger" onClick={myContext.clear}>BORRAR TODO</button>
                         : <h4>Su carrito está vacio</h4>
                     }
                 </div>
@@ -24,8 +62,8 @@ const Cart = () => {
                 <div className="d-flex justify-content-between gap-3">
                     <div className="w-75">
                         {
-                            test.cartList.length > 0
-                            ?test.cartList.map(item => 
+                            myContext.cartList.length > 0
+                            ?myContext.cartList.map(item => 
                             <div key={item.id}>
                                 <div className="card mb-3 border border-end-0 border border-start-0" key={item.id}>
                                     <div className="row g-0">
@@ -37,7 +75,7 @@ const Cart = () => {
                                                 <h5 className="card-title">{item.title}</h5>
                                                 <p className="card-text">Cantidad: {item.quantity} | $ {item.price} c/u</p>
                                                 <h6 className="card-text">$ {item.quantity * item.price}</h6>
-                                                <button type="button" className="btn btn-outline-danger" onClick={() => test.removeItem(item.id)}>Borrar Unidad</button>
+                                                <button type="button" className="btn btn-outline-danger" onClick={() => myContext.removeItem(item.id)}>Borrar Unidad</button>
                                             </div>
                                         </div>
                                     </div>
@@ -49,13 +87,13 @@ const Cart = () => {
                     </div>
                     <div className="w-25">
                         {
-                            test.cartList.length > 0 && 
+                            myContext.cartList.length > 0 && 
                             <div className="card border border-4 border-dark">
                                 <h5 className="card-header">Resumen del pedido</h5>
                                 <div className="card-body">
-                                    <p className="card-text d-flex justify-content-between">Subtotal <span>{`$${test.calcSubTotal()}`} </span></p>
-                                    <h5 className="card-title d-flex justify-content-between">TOTAL <span>{`$${test.calcTotal()}`} </span></h5>
-                                    <button type='button' className="btn btn-dark">Chequear ahora</button>
+                                    <p className="card-text d-flex justify-content-between">Subtotal <span>{`$${myContext.calcSubTotal()}`} </span></p>
+                                    <h5 className="card-title d-flex justify-content-between">TOTAL <span>{`$${myContext.calcTotal()}`} </span></h5>
+                                    <button type='button' className="btn btn-dark" onClick={createOrder}>Chequear ahora</button>
                                 </div>
                             </div>
                         }
